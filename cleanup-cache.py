5b6e9b8d20b6302014cc0326784af034ed2fd2b3
Eric Blau <blau@mcs.anl.gov>
2017-05-16 11:14:21 -0500

    pep8 cleanup, and fixes for dealing with PaginatedResource returned objects

--- cleanup-cache.py
+++ cleanup-cache.py
@@ -29,7 +29,8 @@ def do_client_authentication(client_id, client_secret,
 
     # the useful values that you want at the end of this
     globus_auth_data = token_response.by_resource_server['auth.globus.org']
-    globus_transfer_data = token_response.by_resource_server['transfer.api.globus.org']
+    globus_transfer_data = token_response.by_resource_server[
+        'transfer.api.globus.org']
     globus_auth_token = globus_auth_data['access_token']
     globus_transfer_token = globus_transfer_data['access_token']
 
@@ -40,10 +41,12 @@ def main():
     # start the Native App authentication process
 
     current_time = datetime.utcnow().replace(microsecond=0).isoformat()
-    last_cleanup_time = datetime.utcnow().replace(microsecond=0) - timedelta(hours=24)
+    last_cleanup_time = datetime.utcnow().replace(microsecond=0)\
+        - timedelta(hours=24)
     last_cleanup = last_cleanup_time.isoformat()
     completion_range = last_cleanup+","+current_time
-    print completion_range
+    print "Cleaning up source endpoint {} for outbound transfers completed\
+        in range {}".format(SOURCE_ENDPOINT_ID, completion_range)
 
     transfer_token = do_client_authentication(CLIENT_ID, CLIENT_SECRET)
 
@@ -52,41 +55,62 @@ def main():
 
     # print out a directory listing from an endpoint
     tc.endpoint_autoactivate(SOURCE_ENDPOINT_ID)
-
     try:
-        tasklist = tc.endpoint_manager_task_list(filter_status="SUCCEEDED", filter_endpoint=SOURCE_ENDPOINT_ID, filter_completion_time=completion_range, fields="task_id,source_endpoint,destination_endpoint,source_host_path,owner_string,source_endpoint_id,type")
-        print tasklist
+        tasks = tc.endpoint_manager_task_list(
+                    filter_status="SUCCEEDED",
+                    filter_endpoint=SOURCE_ENDPOINT_ID,
+                    filter_completion_time=completion_range,
+                    fields="task_id,source_endpoint,destination_endpoint,\
+                        source_host_path,owner_string,source_endpoint_id,type")
     except:
-        print "Couldn't get tasklist"
+        print "Couldn't get tasks"
 
-    for task in tasklist:
-        if (task["type"] == "TRANSFER"):
+    tasklist = tasks.data
+    for taskglob in tasklist:
+        if (taskglob.data["type"] == "TRANSFER"):
+            task = taskglob.data
             if (task["source_endpoint_id"] == SOURCE_ENDPOINT_ID):
-                successful_task=tc.endpoint_manager_task_successful_transfers(task["task_id"])
-                print("Task({}): {} -> {}\n was submitted by {}\n{}".  format(task["task_id"], task["source_endpoint"],task["destination_endpoint"], task["owner_string"],task["source_endpoint_id"]))
-                print( "task id is {}",task["task_id"])
+                successful_task = tc.endpoint_manager_task_successful_transfers(
+                    task["task_id"])
+                print("Task({}): {} -> {}\n was submitted by {}\n{}".
+                      format(task["task_id"], task["source_endpoint"],
+                             task["destination_endpoint"],
+                             task["owner_string"],
+                             task["source_endpoint_id"]))
+                print("task id is {}", task["task_id"])
 
-                files_list = [globr["source_path"] for globr in successful_task]
+                files_list = [
+                    globr["source_path"] for globr in successful_task]
                 print "files list is {}".format(files_list)
 
-                #Find the common directory under which all the files live
-                #If one exists, it will be deleted recursively, even if not all
-                #files under it were transferred.
-                #If there is no common directory, each file that was transferred will be deleted
-                commondir=dirname(commonprefix(files_list))
+                # Find the common directory under which all the files live
+                # If one exists, it will be deleted recursively, even if not
+                # all files under it were transferred.
+                # If there is no common directory, each file that was
+                # transferred will be deleted
+                commondir = dirname(commonprefix(files_list))
                 try:
-                    files_on_endpoint = tc.operation_ls(SOURCE_ENDPOINT_ID, path=commondir)
+                    files_on_endpoint = tc.operation_ls(
+                        SOURCE_ENDPOINT_ID, path=commondir)
                 except globus_sdk.exc.TransferAPIError:
-                    print "Directory {} no longer present on source endpoint".format(commondir)
+                    print "Directory {} no longer present on source endpoint,"\
+                        + " there is nothing to delete".format(commondir)
                     continue
 
                 if files_list:
-                    if commondir:    
-                        ddata=globus_sdk.DeleteData(tc, SOURCE_ENDPOINT_ID, label="deletion of {}".format(task["task_id"]), submission_id=None, recursive=True, deadline=None)
+                    if commondir:
+                        ddata = globus_sdk.DeleteData(
+                            tc, SOURCE_ENDPOINT_ID,
+                            label="deletion of {}".format(task["task_id"]),
+                            submission_id=None, recursive=True, deadline=None)
                         ddata.add_item(commondir)
                     else:
-                        ddata=globus_sdk.DeleteData(tc, SOURCE_ENDPOINT_ID, label="deletion of {}".format(task["task_id"]), submission_id=None, recursive=False, deadline=None)
-# If any of the paths are directories, recursive must be set True on the top level DeleteData
+                        ddata = globus_sdk.DeleteData(
+                            tc, SOURCE_ENDPOINT_ID,
+                            label="deletion of {}".format(task["task_id"]),
+                            submission_id=None, recursive=False, deadline=None)
+# If any of the paths are directories, recursive must be set True
+# on the top level DeleteData
                         for path in files_list:
                             ddata.add_item(path)
                     print ddata
@@ -94,25 +118,32 @@ def main():
                     print delete_result
 
                     try:
-                        acl_list=tc.endpoint_manager_acl_list(SOURCE_ENDPOINT_ID)
+                        acl_list = tc.endpoint_manager_acl_list(
+                            SOURCE_ENDPOINT_ID)
                     except:
-                        print "Couldn't get acl list for endpoint {}".format(SOURCE_ENDPOINT_ID)
+                        print "Couldn't get acl list for endpoint {}".\
+                            format(SOURCE_ENDPOINT_ID)
                         continue
                     print acl_list
 
-                    acldict = {i["path"]:i["id"] for i in acl_list}
+                    acldict = {i["path"]: i["id"] for i in acl_list}
 
-                    aclid=""
+                    aclid = ""
 
-                    if acldict[commondir+"/"]:
-                        aclid=acldict[commondir+"/"]
-                        print aclid
+                    try:
+                        aclid = acldict[commondir+"/"]
+                    except:
+                        print "No acl found for directory {}".\
+                            format(commondir+"/")
+                        continue
 
                     if aclid:
                         try:
-                            tc.delete_endpoint_acl_rule(SOURCE_ENDPOINT_ID, aclid)
+                            tc.delete_endpoint_acl_rule(
+                                SOURCE_ENDPOINT_ID, aclid)
                         except:
                             print "Couldn't delete acl rule {}".format(aclid)
+                            continue
 
 if __name__ == '__main__':
     main()
