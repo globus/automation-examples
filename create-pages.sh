861ae91d7b62a5cdb2c77965c8565bf641004042
Netta Gneezy <netta.en@gmail.com>
2019-03-05 11:40:49 -0800

    more progress on create-pages.sh script, also made the script executable

--- create-pages.sh
+++ create-pages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # DESCRIPTION
 #
@@ -6,11 +6,123 @@
 # 2) Copies the index files from `tmp` to `docs/examples/indexgen`
 # 3) Does a `git add`, `git commit`, and `git push` on those (index) files
 
+# Options for generating the index file(s):
+#   directory           The directory in the shared endpoint where the data is located
+#   local-index-dir     Specifies the name of the directory that is created when data is downloaded
+#   no-json             Script will not create an index.json file
+#   html-output         Generates an index.html file
+#   markdown-output     Generates an index.md file
+#   recursive           Generates (smaller) recursive index.html files instead of a single file
+#   dest-endpoint       Index files will be uploaded to this endpoint instead of the shared endpoint
+#   dest-path           Index files will be uploaded to this location/directory
+#   include-filter      Filters the files and directories, all non-specified files/directories are excluded
+#   exclude-filter      Filters the files and directories, excludes all specified files/directories
+#   simple-parser       Downloads the (filtered) data from the shared endpoint, parses the data and generates a file containing the parsed data
+#   case-insensitive    Makes the include/exclude filter(s) be case insensitive
+
+
 # gen_index_dir located in Globus Tutorial Endpoint 1
-SHARED_ENDPOINT='e1e88d04-3c63-11e9-a613-0a54e005f950'
+#SHARED_ENDPOINT='e1e88d04-3c63-11e9-a613-0a54e005f950'
 
-# Request destination (e.g., local) endpoint UUID
-echo "Please enter the UUID for the destination endpoint:"
-read destination_ep
+while [ $# -gt 0 ]; do
+    key="$1"
+    case $1 in
+        --local-endpoint)
+            shift
+            local_endpoint=$1
+            args="--local-endpoint $local_endpoint"
+        ;;
+        --shared-endpoint)
+            shift
+            shared_endpoint=$1
+            args="$args --shared-endpoint $shared_endpoint"
+        ;;
+        --directory)
+            shift
+            args="$args --directory $1"
+        ;;
+        --local-index-dir)
+            shift
+            local_index_dir=$1
+            if [ "$local_index_dir" != "tmp"]
+                then
+                    args="$args --local-index-dir $local_index_dir"
+            fi
+        ;;
+        --no-json)
+            args="$args --no-json"
+        ;;
+        --html-output)
+            html_output=true
+            args="$args --html-output"
+        ;;
+        --markdown-output)
+            args="$args --markdown-output"
+        ;;
+        --recursive)
+            # no reason to use 'recursive' flag if html output is not enabled
+            if [ ! -z "$html_output" ]
+                then
+                    args="$args --recursive"
+            fi
+        ;;
+        --dest-endpoint)
+            shift
+            dest_endpoint=$1
+        ;;
+        --dest-path)
+            if [ ! -z "$dest_endpoint" ]
+                then
+                    shift
+                    args="$args --dest-endpoint $dest_endpoint --dest-path $1"
+            fi
+        ;;
+        --include-filter)
+            shift
+            include_filter=$1
+            args="$args --include-filter $include_filter"
+        ;;
+        --exclude-filter)
+            shift
+            exclude_filter=$1
+            args="$args --exclude-filter $exclude_filter"
+        ;;
+        --simple-parser)
+            args="$args --simple-parser"
+        ;;
+        --case-insensitive)
+            # if there are no filters then there is no need to add the 'case-insensitive' flag
+            if [ ! -z "$include_filter" ] || [ ! -z "$exclude_filter" ]
+                then
+                    args="$args --case-insensitive"
+            fi
+        ;;
+        *)
+            echo ''
+            echo "Error: Unknown Option: '$1'"
+            echo ''
+            exit 1
+    esac
+    shift
+done
 
-function
+# Check that local and shared endpoints are given
+if [ -z $local_endpoint ]
+    then
+        echo 'Error: Local endpoint is not defined' >&2
+        exit 1
+fi
+if [ -z $shared_endpoint ]
+    then
+        echo 'Error: Shared destination endpoint is not defined' >&2
+        exit 1
+fi
+
+# Basic case: no optional arguments given, will generate a single index.json file
+if [ ! -z "$args" ]
+    then
+        ./gen_index.py $args
+    else
+        echo "args failed: running basic script command (local and shared endpoint arguments only)"
+        ./gen_index.py --local-endpoint $local_endpoint --shared-endpoint $shared_endpoint
+fi
